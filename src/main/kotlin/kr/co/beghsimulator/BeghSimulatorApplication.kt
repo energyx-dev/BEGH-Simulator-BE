0550bc1349775142293ad586e8e813db5ba4cfcc
ksh <ksh7412.dev@gmail.com>
2025-02-12 14:27:30 +0900

    feat: 프로젝트 초기 환경 세팅

---
+++ src/main/kotlin/kr/co/beghsimulator/BeghSimulatorApplication.kt
@@ -0,0 +1,11 @@
+package kr.co.beghsimulator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BeghSimulatorApplication
+
+fun main(args: Array<String>) {
+    runApplication<BeghSimulatorApplication>(*args)
+}
---
+++ src/test/kotlin/kr/co/beghsimulator/BeghSimulatorApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.co.beghsimulator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BeghSimulatorApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
