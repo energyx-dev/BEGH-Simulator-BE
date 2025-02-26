444ff29cf1dcc0127803c29479aae4a3c158df89
ksh <ksh7412.dev@gmail.com>
2025-02-26 17:22:33 +0900

    feat: 누락된 파일 및 설정 파일 추가

--- src/main/kotlin/kr/co/beghsimulator/config/ObjectMapperConfig.kt
+++ src/main/kotlin/kr/co/beghsimulator/config/ObjectMapperConfig.kt
@@ -2,6 +2,7 @@ package kr.co.beghsimulator.config
 
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -11,6 +12,8 @@ class ObjectMapperConfig {
 
     @Bean
     fun objectMapper() : ObjectMapper {
-        return jacksonObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+        return jacksonObjectMapper()
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            .setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
     }
 }
