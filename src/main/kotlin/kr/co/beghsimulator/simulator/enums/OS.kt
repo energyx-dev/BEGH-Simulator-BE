f5c9c925e72ca28d3ef96cfd7e33daee4e0ef4a7
ksh <ksh7412.dev@gmail.com>
2025-03-17 17:38:20 +0900

    feat: 코드 리뷰 내용 반영(비동기 방식 적용, os 관련 코드 리펙토링)

--- src/main/kotlin/kr/co/beghsimulator/simulator/enums/OS.kt
+++ src/main/kotlin/kr/co/beghsimulator/simulator/enums/OS.kt
@@ -2,5 +2,16 @@ package kr.co.beghsimulator.simulator.enums
 
 enum class OS {
     WIN,
-    MAC
+    MAC;
+
+    companion object {
+        fun getOS(): OS {
+            val osName = System.getProperty("os.name").lowercase()
+            when {
+                osName.contains("win") -> return WIN
+                osName.contains("mac") -> return MAC
+                else -> throw RuntimeException("win 배포 환경, mac 개발 환경에서만 실행이 가능합니다.")
+            }
+        }
+    }
 }
